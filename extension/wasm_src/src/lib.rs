4bb024b7f08f13db56745f5125e1f6138d2f7b65
Lukas Lueg <lukas.lueg@gmail.com>
2019-07-09 16:24:58 +0200

    Bump to 2018-edition

--- extension/wasm_src/src/lib.rs
+++ extension/wasm_src/src/lib.rs
@@ -1,12 +1,11 @@
-extern crate railroad;
-extern crate macro_railroad;
+#[cfg(cargo_web)]
 #[macro_use]
 extern crate stdweb;
-extern crate syn;
-extern crate htmlescape;
 #[macro_use]
+
 extern crate serde_derive;
 
+#[cfg(cargo_web)]
 use stdweb::js_export;
 
 #[allow(dead_code)]
@@ -15,35 +14,58 @@ mod built_info {
 }
 
 #[derive(Serialize)]
-enum DiagramResult {
-    Ok { name: String, width: i64, height: i64, svg: String },
-    Err(String)
+pub enum DiagramResult {
+    Ok {
+        name: String,
+        width: i64,
+        height: i64,
+        svg: String,
+    },
+    Err(String),
 }
 
+#[cfg(cargo_web)]
 js_serializable!(DiagramResult);
 
-#[js_export]
-fn version_info() -> String {
-    format!("macro_railroad_ext built {} on {} using {}", built_info::BUILT_TIME_UTC, built_info::RUSTC_VERSION, built_info::DEPENDENCIES_STR)
+#[cfg_attr(cargo_web, js_export)]
+pub fn version_info() -> String {
+    format!(
+        "macro_railroad_ext built {} on {} using {}",
+        built_info::BUILT_TIME_UTC,
+        built_info::RUSTC_VERSION,
+        built_info::DEPENDENCIES_STR
+    )
 }
 
-#[js_export]
-fn to_diagram_node(src: &str, hide_internal: bool, keep_groups:bool, foldcommontails: bool, legend: bool) -> DiagramResult {
+#[cfg_attr(cargo_web, js_export)]
+pub fn to_diagram_node(
+    src: &str,
+    hide_internal: bool,
+    keep_groups: bool,
+    foldcommontails: bool,
+    legend: bool,
+) -> DiagramResult {
     match to_diagram(src, hide_internal, keep_groups, foldcommontails, legend) {
         Ok((name, svg)) => {
             use railroad::RailroadNode;
-            DiagramResult::Ok { name: htmlescape::encode_minimal(&name),
-                                width: svg.width(),
-                                height: svg.height(),
-                                svg: svg.to_string() }
-        },
-        Err(e) => {
-            DiagramResult::Err(e.to_string())
+            DiagramResult::Ok {
+                name: htmlescape::encode_minimal(&name),
+                width: svg.width(),
+                height: svg.height(),
+                svg: svg.to_string(),
+            }
         }
+        Err(e) => DiagramResult::Err(e.to_string()),
     }
 }
 
-fn to_diagram(src: &str, hide_internal: bool, keep_groups: bool, foldcommontails: bool, legend: bool) -> syn::parse::Result<(String, railroad::Diagram<Box<railroad::RailroadNode>>)> {
+fn to_diagram(
+    src: &str,
+    hide_internal: bool,
+    keep_groups: bool,
+    foldcommontails: bool,
+    legend: bool,
+) -> syn::parse::Result<(String, railroad::Diagram<Box<dyn railroad::RailroadNode>>)> {
     let macro_rules = macro_railroad::parser::parse(&src)?;
 
     let mut tree = macro_railroad::lowering::MacroRules::from(macro_rules);
