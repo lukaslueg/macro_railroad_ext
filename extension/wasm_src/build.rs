011d3d23fd30eb5ed9e721dd6fbbc7f1ad043cae
Lukas Lueg <lukas.lueg@gmail.com>
2020-02-22 16:26:45 +0100

    Bump dependencies

--- extension/wasm_src/build.rs
+++ extension/wasm_src/build.rs
@@ -9,6 +9,6 @@ fn main() {
 
     let src = env::var("CARGO_MANIFEST_DIR").unwrap();
     let dst = path::Path::new(&env::var("OUT_DIR").unwrap()).join("built.rs");
-    built::write_built_file_with_opts(&opts, &src, &dst)
+    built::write_built_file_with_opts(&opts, src.as_ref(), &dst)
         .expect("Failed to acquire build-time information.");
 }
