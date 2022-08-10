0cf87cbfa52f25b7ebd65d11272cd87097c91d12
Folyd <lyshuhow@gmail.com>
2022-08-10 10:38:25 +0800

    Fix wasm filename typo

--- extension/content_scripts/main.js
+++ extension/content_scripts/main.js
@@ -8,7 +8,7 @@ async function load() {
     let macros = document.querySelectorAll('pre.macro');
     if (!macros || macros.length === 0) return;
 
-    await wasm_bindgen(chrome.runtime.getURL('wasm/macro-railroad.wasm'));
+    await wasm_bindgen(chrome.runtime.getURL('wasm/macro_railroad_ext.wasm'));
     injectCss();
 
     for (let macro of macros) {
